e4bab0c6f40ae5352f0906e76c4172bddae6d473
Gabriel-Pereira1788 <96449242+Gabriel-Pereira1788@users.noreply.github.com>
2022-01-23 18:45:11 -0300

    Atualização do projeto landing page parallax

---
+++ script.js
@@ -0,0 +1,17 @@
+var botão_abrir=document.querySelector('div#botao_nav')
+var botão_fechar=document.querySelector('div#botão_fechar')
+var cabeçalho=document.querySelector('header#cabeçalho')
+var nav_mobile=document.querySelector('div.nav_int')
+function clicar(){
+    cabeçalho.style.height='500px'
+    nav_mobile.style.width='100%'
+    nav_mobile.style.display='block'
+    nav_mobile.style.content='normal'
+    nav_mobile.style.position='static'
+    botão_fechar.style.display='inline-block'
+}
+function fechar(){
+    cabeçalho.style.height='90px'
+    nav_mobile.style.display='none'
+    botão_fechar.style.display='none'
+}
