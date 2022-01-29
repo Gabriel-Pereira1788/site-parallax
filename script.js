44152e7168fce273e4a5cf04ea7bd2b75736bfbd
Gabriel-Pereira1788 <96449242+Gabriel-Pereira1788@users.noreply.github.com>
2022-01-29 12:19:27 -0300

    projeto finalizado.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var botão_abrir=document.querySelector('div#botao_nav')
+var botão_abrir=document.querySelector('div#botão_nav')
 var botão_fechar=document.querySelector('div#botão_fechar')
 var cabeçalho=document.querySelector('header#cabeçalho')
 var nav_mobile=document.querySelector('div.nav_int')
 function clicar(){
-    cabeçalho.style.height='500px'
+    cabeçalho.style.height='400px'
     nav_mobile.style.width='100%'
     nav_mobile.style.display='block'
     nav_mobile.style.content='normal'
@@ -11,7 +11,7 @@ function clicar(){
     botão_fechar.style.display='inline-block'
 }
 function fechar(){
-    cabeçalho.style.height='90px'
+    cabeçalho.style.height='70px'
     nav_mobile.style.display='none'
     botão_fechar.style.display='none'
 }
